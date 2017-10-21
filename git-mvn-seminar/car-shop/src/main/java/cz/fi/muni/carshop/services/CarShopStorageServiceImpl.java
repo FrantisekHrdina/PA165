bed3e66e87a15ce75818f62b00e6d1db9a62ce7b
František Hrdina <franta.hrdina@mail.muni.cz>
2017-10-21 11:03:20 +0200

    Task03
    junit to 4.11
    fixed method for adding car with negative prize
    implemented method sellCar

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -7,9 +7,11 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.sun.org.apache.regexp.internal.RE;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -29,7 +31,31 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 	}
 
 	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		if (car == null){
+			throw new RequestedCarNotFoundException("Given car is null");
+		}
+
+		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+
+		if (!allCars.containsKey(car.getType())){
+			throw new RequestedCarNotFoundException("Type of given car was not found");
+		}
+
+		List<Car> carsWithSameType = allCars.get(car.getType());
+
+		if (!carsWithSameType.contains(car)){
+			throw new RequestedCarNotFoundException("Given Car was not found");
+		}
+
+		carsWithSameType.remove(car);
+	}
+
+	@Override
 	public void addCarToStorage(Car car) {
+		if (car.getPrice() < 0){
+			throw new IllegalArgumentException("Prize of car is negative");
+		}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
 
